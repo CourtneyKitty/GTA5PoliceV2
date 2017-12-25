f1ecf91e6b62cb5037fee5e3713f7852c01035a5
byBlurr <dbldumont11@gmail.com>
2017-12-25 18:31:13 +0000

    Add project files.

---
+++ GTA5PoliceV2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GTA5PoliceV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
